fdd2b27a027eaf75594cc9cb2c156dfaeaa7fbeb
Cezary Baginski <cezary.baginski@gmail.com>
2010-05-06 14:49:42 +0200

    Fixes for sh

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-#setup vi as priority 1!!
+set -e
 
 BOOTSTRAP_DIR=$(pwd)
 
+#setup vi as priority 1!!
 VIMRC=${HOME}/.vimrc
 if [[ -e $VIMRC ]]; then
   cmp $VIMRC dotfiles/vimrc || vimdiff $VIMRC dotfiles/vimrc
 else
   cp dotfiles/vimrc $VIMRC
 fi
+
+#setup bootstrap in ~/bin
+BIN_DIR=${HOME}/bin
+mkdir -p ${BIN_DIR}
+[[ -e ${BIN_DIR}/bootstrap ]] || ln -s ./bootstrap ${BIN_DIR}
