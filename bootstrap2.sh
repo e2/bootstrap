cd4139c11b15096c7054970acd58d08d6a1e6639
Cezary Baginski <cezary.baginski@gmail.com>
2010-05-06 18:44:31 +0200

    Added dotfiles and fixed bootstrap

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -4,11 +4,11 @@ BOOTSTRAP_DIR=$HOME/git/bootstrap
 
 install_from_template()
 {
-	DST=$HOME/.$1
-	TEMPLATE=dotfiles/$1
+	DST="$HOME/.$1"
+	TEMPLATE="dotfiles/$1"
 	
 	if [[ -e $DST ]]; then
-		cmp "$DST $TEMPLATE" || vimdiff "$DST $TEMPLATE"
+		cmp "$DST" "$TEMPLATE" || vimdiff "$DST" "$TEMPLATE"
 	else
 		cp "$TEMPLATE" "$DST"
 	fi
