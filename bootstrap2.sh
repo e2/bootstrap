d93a90db53ce11eb11c4ecbee2a41c73d66b0a1d
Cezary Baginski <cezary.baginski@gmail.com>
2010-05-06 16:50:39 +0200

    Added the idea of setup as a workaround

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -4,15 +4,6 @@ set -e
 
 BOOTSTRAP_DIR=$(pwd)
 
-#setup bootstrap in ~/bin
-BIN_DIR=${HOME}/bin
-mkdir -p ${BIN_DIR}
-if [[ ! -e ${BIN_DIR}/bootstrap ]]; then
-  ln -s ${BOOTSTRAP_DIR}/bootstrap ${BIN_DIR}
-  echo Ok, bootstrap installed in ${BIN_DIR}
-  echo Now, run the following: ${BIN_DIR}/bootstrap
-  exit
-fi
 
 #setup vi as priority one!!
 VIMRC=${HOME}/.vimrc
