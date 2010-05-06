e42ed7562ce8272882b6b07b096a12ae5573c231
Cezary Baginski <cezary.baginski@gmail.com>
2010-05-06 18:10:39 +0200

    added dotfiles

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
+#!/bin/zsh
 
-set -e
+BOOTSTRAP_DIR=$HOME/git/bootstrap
 
-BOOTSTRAP_DIR=$(pwd)
+install_from_template()
+{
+	DST=$HOME/.$1
+	TEMPLATE=dotfiles/$1
+	
+	if [[ -e $DST ]]; then
+		cmp "$DST $TEMPLATE" || vimdiff "$DST $TEMPLATE"
+	else
+		cp "$TEMPLATE" "$DST"
+	fi
+}
 
-
-#setup vi as priority one!!
-VIMRC=${HOME}/.vimrc
-if [[ -e $VIMRC ]]; then
-  cmp $VIMRC dotfiles/vimrc || vimdiff $VIMRC dotfiles/vimrc
-else
-  cp dotfiles/vimrc $VIMRC
-fi
-
+install_from_template vimrc
+install_from_template gitconfig 
+install_from_template gitignore
+install_from_template bashrc 
+install_from_template bash_profile
+install_from_template zshrc 
